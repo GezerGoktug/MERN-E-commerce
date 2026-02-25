25d020fc4362c0356477e6307788d83a24df5720
Göktuğ Gezer <ggezer780@gmail.com>
2026-02-25 22:09:14 +0300

    chore: refactor frontend folder name

--- frontend-monorepo/packages/apps/main/src/helper/createRatingArray.ts
+++
@@ -1,8 +0,0 @@
-export const createRatingArray = (rating: number): number[] => {
-  return [0, 0, 0, 0, 0].map((item, i) => {
-    if (i + 1 <= rating) {
-      return 1;
-    }
-    return item;
-  });
-};
--- frontend/src/store/theme/actions.ts
+++
@@ -1,3 +0,0 @@
-import themeStore, { ThemeType } from "./themeStore";
-
-export const setTheme = (theme: ThemeType) => themeStore.getState().__setTheme(theme);

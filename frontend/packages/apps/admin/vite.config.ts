31a26fb47df1f502f7a75d8770803e9b079dcb9c
Göktuğ Gezer <ggezer780@gmail.com>
2026-03-08 18:21:01 +0300

    chore: replaced  preview settings for prod env and added new start scripts for this

--- frontend/packages/apps/admin/vite.config.ts
+++ frontend/packages/apps/admin/vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/admin/",
+  base: "/admin/",
   server: {
+    port: 3001
+  },
+  preview: {
     port: 3001
   },
   build: {

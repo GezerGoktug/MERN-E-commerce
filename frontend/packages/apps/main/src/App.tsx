06922a27c4c8dcca77ed89c2760741a52c8efc85
Göktuğ Gezer <ggezer780@gmail.com>
2026-03-15 23:02:46 +0300

    perf: changed route components to lazy loading components

--- frontend/packages/apps/main/src/App.tsx
+++ frontend/packages/apps/main/src/App.tsx
@@ -1,21 +1,25 @@
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
-import Error from "./pages/Error/Error";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Cart from "./pages/Cart/Cart";
-import Auth from "./pages/Auth";
-import Collections from "./pages/Collections/Collections";
-import ProductDetail from "./pages/ProductDetail/ProductDetail";
-import PlaceOrder from "./pages/PlaceOrder";
-import Profile from "./pages/Profile";
-import { Toaster } from "react-hot-toast";
-import { isAccess } from "./store/auth/hooks";
-import PaymentResult from "./pages/PaymentResult/PaymentResult";
-import Favourites from "./pages/Favourites";
 import ErrorBoundary from "./ErrorBoundary";
 import AuthGuard from "./guards/AuthGuard";
+import { Toaster } from "react-hot-toast";
+import { isAccess } from "./store/auth/hooks";
+import { lazy, Suspense } from "react";
+import { Loading } from "@forever/ui-kit";
+
+const Home = lazy(() => import("./pages/Home"));
+const Error = lazy(() => import("./pages/Error/Error"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Cart = lazy(() => import("./pages/Cart/Cart"));
+const Auth = lazy(() => import("./pages/Auth"));
+const Collections = lazy(() => import("./pages/Collections/Collections"));
+const ProductDetail = lazy(() => import("./pages/ProductDetail/ProductDetail"));
+const PlaceOrder = lazy(() => import("./pages/PlaceOrder"));
+const Profile = lazy(() => import("./pages/Profile"));
+const PaymentResult = lazy(() => import("./pages/PaymentResult/PaymentResult"));
+const Favourites = lazy(() => import("./pages/Favourites"));
+
 
 const PrivateRoute = () => {
   return isAccess() ? <Outlet /> : <Navigate to="/auth" />;
@@ -25,30 +29,32 @@ function App() {
   return (
     <ErrorBoundary>
       <Toaster position="top-right" reverseOrder={false} />
-      <AuthGuard>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/collection" element={<Collections />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/place-order" element={<PlaceOrder />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route element={<PrivateRoute />}>
-              <Route path="/favourite" element={<Favourites />} />
+      <Suspense fallback={<Loading />}>
+        <AuthGuard>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/collection" element={<Collections />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/place-order" element={<PlaceOrder />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/favourite" element={<Favourites />} />
+              </Route>
+              <Route element={<PrivateRoute />}>
+                <Route path="/payment/result" element={<PaymentResult />} />
+              </Route>
+              <Route element={<PrivateRoute />}>
+                <Route path="/profile" element={<Profile />} />
+              </Route>
+              <Route path="/*" element={<Error />} />
             </Route>
-            <Route element={<PrivateRoute />}>
-              <Route path="/payment/result" element={<PaymentResult />} />
-            </Route>
-            <Route element={<PrivateRoute />}>
-              <Route path="/profile" element={<Profile />} />
-            </Route>
-            <Route path="/*" element={<Error />} />
-          </Route>
-        </Routes>
-      </AuthGuard>
+          </Routes>
+        </AuthGuard>
+      </Suspense>
     </ErrorBoundary>
   );
 }

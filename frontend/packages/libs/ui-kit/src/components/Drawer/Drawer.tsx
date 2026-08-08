3ad04224c9febc891b2e17bfdf24bb9234f6c5eb
Göktuğ Gezer <ggezer780@gmail.com>
2026-08-08 20:53:02 +0300

    fix: resolved build error because of missing type prefix at type import

--- frontend/packages/libs/ui-kit/src/components/Drawer/Drawer.tsx
+++ frontend/packages/libs/ui-kit/src/components/Drawer/Drawer.tsx
@@ -3,7 +3,7 @@ import Backdrop from "../Backdrop/Backdrop";
 import Overlay from "../Overlay/Overlay";
 import { AnimatePresence, motion, useDragControls } from "framer-motion";
 import { OutsideClickHandler } from "@forever/common-utils";
-import { ReactNode, useRef, useState } from "react";
+import { type ReactNode, useRef, useState } from "react";
 import clsx from "clsx";
 import { MdOutlineClose } from "react-icons/md";
 
